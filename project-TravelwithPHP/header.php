3b967937807408c22af2de61180478d04cdfb6e3
Nha Triet <91239082+nhatriet@users.noreply.github.com>
2023-04-26 14:58:53 +0700

    update link

--- project-TravelwithPHP/header.php
+++ project-TravelwithPHP/header.php
@@ -8,7 +8,6 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>F5-Travel</title>
-
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/css/books.css">
     <link rel="stylesheet" href="./assets/css/blog.css">
@@ -17,6 +16,12 @@ session_start();
     <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="project.js">
+	<link rel="stylesheet" type="text/css" href="./assets/css/contact.css">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./assets/css/contact_responsive.css">
 
 </head>
 <body>
